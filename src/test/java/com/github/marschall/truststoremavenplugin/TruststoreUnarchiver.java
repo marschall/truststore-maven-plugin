a97a126bd5b7d267a1e88ed8bcd08b3503be3e53
Philippe Marschall <philippe.marschall@gmail.com>
2018-08-08 16:51:34 +0200

    directly write to stream

--- src/test/java/com/github/marschall/truststoremavenplugin/TruststoreUnarchiver.java
+++ src/test/java/com/github/marschall/truststoremavenplugin/TruststoreUnarchiver.java
@@ -1,7 +1,8 @@
 package com.github.marschall.truststoremavenplugin;
 
 import java.io.IOException;
-import java.io.Writer;
+import java.io.OutputStream;
+import java.nio.charset.StandardCharsets;
 import java.security.GeneralSecurityException;
 import java.security.KeyStore;
 import java.security.cert.Certificate;
@@ -11,24 +12,24 @@ import java.util.Base64.Encoder;
 import java.util.Enumeration;
 
 public class TruststoreUnarchiver {
-  
+
   private void unarchive(KeyStore keyStore) throws GeneralSecurityException, IOException {
     Enumeration<String> aliases = keyStore.aliases();
     while (aliases.hasMoreElements()) {
       String alias = aliases.nextElement();
       if (keyStore.isCertificateEntry(alias)) {
-        Writer writer = null;
-        unarchive(alias, keyStore.getCertificate(alias), writer);
+        OutputStream outputStream = null;
+        unarchive(alias, keyStore.getCertificate(alias), outputStream);
       }
     }
   }
 
-  private void unarchive(String alias, Certificate certificate, Writer writer) throws IOException, CertificateEncodingException {
+  private void unarchive(String alias, Certificate certificate, OutputStream outputStream) throws IOException, CertificateEncodingException {
     // writer.println(X509Factory.BEGIN_CERT);
-    writer.write("-----BEGIN CERTIFICATE-----\n");
+    outputStream.write("-----BEGIN CERTIFICATE-----\n".getBytes(StandardCharsets.US_ASCII));
     Encoder encoder = Base64.getEncoder();
-    writer.write(encoder.encodeToString(certificate.getEncoded()));
-    writer.write("-----END CERTIFICATE-----");
+    encoder.wrap(outputStream).write(certificate.getEncoded());
+    outputStream.write("-----END CERTIFICATE-----".getBytes(StandardCharsets.US_ASCII));
     //writer.println(X509Factory.END_CERT);
   }
 
